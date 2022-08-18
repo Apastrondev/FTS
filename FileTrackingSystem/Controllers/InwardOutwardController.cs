84ee438fbf0181d5c1ca836174dfc0bf905b1966
apastrondevs@gmail.com <apastrondevs@gmail.com>
2022-08-18 21:03:28 +0530

    Bhagyashree

---
+++ FileTrackingSystem/Controllers/InwardOutwardController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileTrackingSystem.Controllers
+{
+    public class InwardOutwardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult FromTo()
+        {
+            return View();
+        }
+        public IActionResult SubjectMaster()
+        {
+            return View();
+        }
+        public IActionResult DepartmentMaster()
+        {
+            return View();
+        }
+        public IActionResult LetterType()
+        {
+            return View();
+        }
+        public IActionResult StatusMaster()
+        {
+            return View();
+        }
+    }
+}
